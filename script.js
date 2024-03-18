a4ec4a6204c35e38fcf8f975553901a7dcc07db9
Gabriel de Jesus <contatogabrieldejesusaraujo@gmail.com>
2024-03-18 11:41:55 -0300

    four commit

--- script.js
+++ script.js
@@ -4,21 +4,42 @@ const curtoButton = document.querySelector('.app__card-button--curto')
 const longoButton = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseButton = document.querySelector('#start-pause')
+const musicaFocoImput = document.querySelector('#alternar-musica')
+const musica = new Audio('sons/luna-rise-part-one.mp3')
+musica.loop = true
 
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
+musicaFocoImput.addEventListener('change', () => {
+    if (musica.paused){
+        musica.play()
+    }else(
+        musica.pause()
+    )
+})
 
 focoButton.addEventListener('click', () =>{
     alterarContexto('foco')
+    focoButton.classList.add('active')
 })
 
 curtoButton.addEventListener('click', () =>{
    alterarContexto('descanso-curto')
+   curtoButton.classList.add('active')
 })
 
 longoButton.addEventListener('click', () =>{
     alterarContexto('descanso-longo')
+    longoButton.classList.add('active')
 })
 
 function alterarContexto(contexto){
+    botoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -45,6 +66,31 @@ function alterarContexto(contexto){
     }
 }
 
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <=0){
+        zerar()
+        alert('Tempo finalizado!')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log(tempoDecorridoEmSegundos)
+
+}
+
+startPauseButton.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar(){
+    if(intervaloId){
+        zerar()
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar(){
+    clearInterval(intervaloId)
+    intervaloId = null
+}
 
 
 
